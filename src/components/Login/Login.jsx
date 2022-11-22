994fd6179f6b47b861f382f3a9a5a7cf6b9c0019
tienak1 <thattien1111@gmail.com>
2022-11-23 05:22:41 +0700

    feat: add call auth api, update login component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,98 +1,212 @@
-import React from "react";
+import React, { Component } from "react";
+import { Navigate } from 'react-router-dom';
 
-export default function Login() {
-  const handleLogin = (e) => {
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+
+import { connect } from "react-redux";
+import { login } from "../../actions/auth";
+
+const required = (value) => {
+  if (!value) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This field is required!
+      </div>
+    );
+  }
+};
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+
+    this.state = {
+      email: "",
+      password: "",
+      loading: false,
+    };
+  }
+
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value,
+    });
+  }
+
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+
+  handleLogin(e) {
     e.preventDefault();
-    console.log(e.target);
-  };
-  return (
-    <div className="container-fluid" style={{ backgroundColor: "#eaeaea" }}>
-      <div className="row">
-        <div
-          className="card qz-card qz-card-rounded qz-card-shadow w-25 p-3 "
-          style={{
-            margin: "0 auto",
-            border: "none",
-            borderRadius: "0px",
-            marginTop: "0.5%",
-            marginBottom: "0.5%",
-          }}
-        >
-          <h3 className="text-center" style={{ fontWeight: "bold" }}>
-            Log in
-          </h3>
-          <form>
-            <div className="form-group my-3">
-              <label className="qz-form-label" htmlFor="exampleInputEmail1">
-                Your e-mail
-              </label>
-              <input
-                type="email"
-                formcontrolname="email"
-                className="form-control qz-text-input"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                placeholder="name@domain.com"
-              />
-              {/* <div>
-                <small className="text-danger mx-2">ID is required</small>
-                <small className="text-danger mx-2">
-                  Enter valid E-mail ID.
-                </small>
-              </div> */}
-            </div>
-            <div className="form-group">
-              <label className="qz-form-label" htmlFor="exampleInputPassword1">
-                Password
-              </label>
-              <input
-                type="password"
-                formcontrolname="password"
-                className="form-control qz-text-input"
-                id="exampleInputPassword1"
-                placeholder="at least 6 characters"
-              />
-              {/* <div>
-                <small className="text-danger mx-2">
-                  Password is required.
-                </small>
-                <small className="text-danger mx-2">
-                  Password must be at least 8 characters.
-                </small>
-              </div> */}
-            </div>
 
-            <button
-              type="submit"
-              className="btn btn-success w-100 mt-4"
-              onClick={(e) => handleLogin(e)}
-            >
+    this.setState({
+      loading: true,
+    });
+
+    this.form.validateAll();
+
+    const { dispatch, history } = this.props;
+
+    if (this.checkBtn.context._errors.length === 0) {
+      dispatch(login(this.state.email, this.state.password))
+        .then(() => {
+          history.push("/"); // TODO: redirect to home page
+          window.location.reload();
+        })
+        .catch(() => {
+          this.setState({
+            loading: false
+          });
+        });
+    } else {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
+
+  render() {
+    const { isLoggedIn, message } = this.props;
+
+    if (isLoggedIn) {
+      return <Navigate to="/" />; // TODO: redirect to home page
+    }
+
+    return (
+      <div className="container-fluid" style={{ backgroundColor: "#eaeaea" }}>
+        <div className="row">
+          <div
+            className="card qz-card qz-card-rounded qz-card-shadow w-25 p-3 "
+            style={{
+              margin: "0 auto",
+              border: "none",
+              borderRadius: "0px",
+              marginTop: "0.5%",
+              marginBottom: "0.5%",
+            }}
+          >
+            <h3 className="text-center" style={{ fontWeight: "bold" }}>
               Log in
-            </button>
-            <h6 className="text-center my-2">or</h6>
-            <button
-              type="submit"
-              className="btn btn-white w-100"
-              style={{ border: "1px solid black" }}
+            </h3>
+            <Form
+              onSubmit={this.handleLogin}
+              ref={(c) => {
+                this.form = c;
+              }}
             >
-              <img
-                src="https://play-lh.googleusercontent.com/aFWiT2lTa9CYBpyPjfgfNHd0r5puwKRGj2rHpdPTNrz2N9LXgN_MbLjePd1OTc0E8Rl1"
-                alt=""
-                style={{ width: "10%" }}
+              <div className="form-group my-3">
+                <label className="qz-form-label" htmlFor="exampleInputEmail1">
+                  Your e-mail
+                </label>
+                <Input
+                  type="text"
+                  formcontrolname="email"
+                  className="form-control qz-text-input"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="name@domain.com"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.onChangeEmail}
+                  validations={[required]}
+                />
+                {/* <div>
+                  <small className="text-danger mx-2">ID is required</small>
+                  <small className="text-danger mx-2">
+                    Enter valid E-mail ID.
+                  </small>
+                </div> */}
+              </div>
+              <div className="form-group">
+                <label className="qz-form-label" htmlFor="exampleInputPassword1">
+                  Password
+                </label>
+                <Input
+                  type="password"
+                  formcontrolname="password"
+                  className="form-control qz-text-input"
+                  id="exampleInputPassword1"
+                  placeholder="at least 6 characters"
+                  value={this.state.password}
+                  onChange={this.onChangePassword}
+                  validations={[required]}
+                />
+                {/* <div>
+                  <small className="text-danger mx-2">
+                    Password is required.
+                  </small>
+                  <small className="text-danger mx-2">
+                    Password must be at least 8 characters.
+                  </small>
+                </div> */}
+              </div>
+
+              <button
+                disabled={this.state.loading}
+                className="btn btn-success w-100 mt-4"
+                
+              >
+                {this.state.loading && (
+                  <span className="spinner-border spinner-border-sm"></span>
+                )}
+                Log in
+              </button>
+              {message && (
+                <div className="form-group">
+                  <div className="alert alert-danger" role="alert">
+                    {message}
+                  </div>
+                </div>
+              )}
+              <CheckButton
+                style={{ display: "none" }}
+                ref={(c) => {
+                  this.checkBtn = c;
+                }}
               />
-              Continue With Google
-            </button>
-            <div className="mt-3 float-right text-center">
-              <p>
-                Don't have an account?&nbsp;
-                <a className="font-weight-bold" href="/signup">
-                  Sign up
-                </a>
-              </p>
-            </div>
-          </form>
+              <h6 className="text-center my-2">or</h6>
+              <button
+                type="submit"
+                className="btn btn-white w-100"
+                style={{ border: "1px solid black" }}
+              >
+                <img
+                  src="https://play-lh.googleusercontent.com/aFWiT2lTa9CYBpyPjfgfNHd0r5puwKRGj2rHpdPTNrz2N9LXgN_MbLjePd1OTc0E8Rl1"
+                  alt=""
+                  style={{ width: "10%" }}
+                />
+                Continue With Google
+              </button>
+              <div className="mt-3 float-right text-center">
+                <p>
+                  Don't have an account?&nbsp;
+                  <a className="font-weight-bold" href="/signup">
+                    Sign up
+                  </a>
+                </p>
+              </div>
+            </Form>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+function mapStateToProps(state) {
+  const { isLoggedIn } = state.auth;
+  const { message } = state.message;
+  return {
+    isLoggedIn,
+    message
+  };
+}
+
+export default connect(mapStateToProps)(Login);
